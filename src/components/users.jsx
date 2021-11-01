a22cfcd4fed69470f258d1469c5320df822e91c5
raisa khaliulina <evadar31@gmail.com>
2021-11-01 03:13:15 +0300

    Initial commit

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,96 +1,52 @@
-import React, { useState } from "react"
-import api from "../API"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { paginate } from "../utils/paginate";
+import Pagination from "./pagination";
+import User from "./user";
 
+const Users = ({ users: allUsers, ...rest }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const count = allUsers.length;
+    const pageSize = 4;
 
-const Users = () => {
-  
-  const [users, setUsers] = useState(api.users.fetchAll())
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+        console.log("page: ", pageIndex);
+    };
+    const users= paginate(allUsers, currentPage, pageSize);
+    return (
+        <>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <User {...rest} {...user} key={user._id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
+};
+Users.propTypes = {
+    users: PropTypes.array
+};
 
-  
-  const handleDelete = (userId) => {
-    setUsers(users.filter((users) => users._id !== userId.target.id))
-  }
-
-  
-  const renderPhrase = (number) => {
-    const lastTwoDigitsOfNumber = number % 100
-    const lastDigitOfNumber = number % 10
-    if (number === 0) return `Никто с тобой не тусанет`
-    if (
-       (lastTwoDigitsOfNumber > 11 && lastTwoDigitsOfNumber < 15) ||
-       lastDigitOfNumber === 1
-       )
-       return `${users.length} человек тусанет с тобой сегодня`
-    if (lastDigitOfNumber > 1 && lastDigitOfNumber < 5)
-       return `${users.length} человека тусанут с тобой сегодня`
-       return `${users.length} человек тусанет с тобой сегодня`
-  }
-
-  const getTextClasses = (value) => {
-    let classes = "badge m-2 bg-"
-    if (value === 0) return (classes += "danger")
-    if (value > 0) return (classes += "primary")
-    return (classes += value)
-  }
-
-  const displayTable = (value) => {
-    let classes = "table table-hover"
-    if (!value) return (classes += " d-none")
-    return classes
-  }
-
-  return (
-    <>
-      <span className={getTextClasses(users.length)}>
-      {renderPhrase(users.length)}
-      </span>
-      <table className={displayTable(users.length)}>
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id}>
-              <td key={user.name}>{user.name}</td>
-              <td>
-                {user.qualities.map((quality) => (
-                  <span
-                    key={user.name + quality._id}
-                    className={getTextClasses(quality.color)}
-                  >
-                    {quality.name}
-                  </span>
-                ))}
-              </td>
-              <td key={user.name + user.profession._id}>
-                {user.profession.name}
-              </td>
-              <td key={user.name + user.completedMeetings}>
-                {user.completedMeetings}
-              </td>
-              <td key={user.name + user.rate}>{user.rate} /5</td>
-              <td>
-                <button
-                  id={user._id}
-                  onClick={handleDelete}
-                  className={"badge bg-danger"}
-                >
-                  delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  )
-}
-
-export default Users
+export default Users;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
-import Users from "./components/users.jsx"
+import App from "./app.jsx"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Users />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
